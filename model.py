ef120a5a05ec26147e73280b2a73af5d2e6ab449
kareem <karim.salah2048@gmail.com>
2018-04-26 02:03:24 +0200

    remove the knn and svm functions

--- model.py
+++ model.py
@@ -15,8 +15,6 @@ def get_model(model: str = 'CNN') -> Sequential:
     saved_weights_name = model + "_" + "Weights.h5"
     return {
         'CNN': get_cnn,
-        'KNN': get_knn,
-        'SVM': get_svm,
     }[model]()
 
 
@@ -58,37 +56,3 @@ def get_cnn() -> Sequential:
     return model
 
 
-def get_knn() -> Sequential:
-    """
-
-    :rtype: Sequential
-    """
-    model = Sequential()
-
-    # TODO: Build KNN model
-    model.add(Conv2D(32, 5, activation='relu', input_shape=images_shape))
-    model.add(MaxPooling2D(2))
-    model.add(Conv2D(64, 5, activation='relu'))
-    model.add(MaxPooling2D(2))
-    model.add(Conv2D(128, 3, activation='relu'))
-    model.add(MaxPooling2D(2))
-    model.add(Conv2D(256, 2, activation='relu'))
-    model.add(MaxPooling2D(2))
-    model.add(Flatten())
-    model.add(Dense(32,kernel_initializer='normal', activation='relu'))
-    model.add(Dense(1, kernel_initializer='normal'))
-    model.add(Dense(len(DATA_SET_COLS)))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    return model
-
-
-def get_svm() -> Sequential:
-    """
-
-    :rtype: Sequential
-    """
-    model = Sequential()
-
-    # TODO: Build SVM model
-
-    return model
